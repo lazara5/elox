bd333124b6a5dbaa5d758ad59b0d49b7e4be2d48
Adrian Lazar <lazara5@users.noreply.github.com>
2023-01-28 12:53:48 +0200

    Update functional test framework

--- tests/eloxTests.c
+++ tests/eloxTests.c
@@ -6,29 +6,33 @@
 #include "elox/util.h"
 #include "elox.h"
 
+#define FUNCTIONAL_TEST(PATH, NAME) \
+	START_TEST(NAME) {\
+		VMCtx vmCtx; \
+		EloxConfig config; \
+		eloxInitConfig(&config); \
+		initVMCtx(&vmCtx, &config); \
+\
+		EloxInterpretResult res = eloxRunFile(&vmCtx, #PATH); \
+		ck_assert_msg(res == ELOX_INTERPRET_OK, "FAIL (%d): %s", res, #PATH); \
+\
+		destroyVMCtx(&vmCtx); \
+	} END_TEST
+
 #include "elox-functional-tests.h"
-
-START_TEST(testFunctional) {
-	VMCtx vmCtx;
-
-	for (size_t ft = 0; ft < ELOX_ARRAY_SIZE(functionalTests); ft++ ) {
-		_mark_point(functionalTests[ft], 0);
-		EloxConfig config;
-		eloxInitConfig(&config);
-		initVMCtx(&vmCtx, &config);
-
-		EloxInterpretResult res = eloxRunFile(&vmCtx, functionalTests[ft]);
-		ck_assert_msg(res == ELOX_INTERPRET_OK, "FAIL (%d): %s", res, functionalTests[ft]);
-
-		destroyVMCtx(&vmCtx);
-	}
-} END_TEST
+#undef FUNCTIONAL_TEST
 
 int main(int argc ELOX_UNUSED, char **argv ELOX_UNUSED) {
 	Suite *s = suite_create("elox");
 
 	TCase *tcFunctional = tcase_create("Functional");
-	tcase_add_test(tcFunctional, testFunctional);
+
+#define FUNCTIONAL_TEST(PATH, NAME) \
+	tcase_add_test(tcFunctional, NAME);
+
+#include "elox-functional-tests.h"
+#undef FUNCTIONAL_TEST
+
 	suite_add_tcase(s, tcFunctional);
 
 	SRunner *sr = srunner_create(s);
